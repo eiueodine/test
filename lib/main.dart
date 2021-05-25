bcd2b1058686457e0853083aefa3b7eb359924ce
Sviatoslav <cdzjckfd>
2021-05-25 14:09:28 +0300

    4 update

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp3());
 }
-
-void main() {
-  runApp(MyApp3());
-}
-void 6666() {
-  runApp(MyApp3());
-}
-
-ukgobbkljn
-nknklj
-    kik
-kubljk
-    jhbkjl
